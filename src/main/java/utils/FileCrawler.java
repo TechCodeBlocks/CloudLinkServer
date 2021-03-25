5c56fac2a0418c06612f7b9cbe560bcd87783d97
Benjamin Solomons <benjamin1905.bs@gmail.com>
2021-03-25 15:11:29 +0000

    Created basic file crawler - needs to be tested and fully implemented with functions to handle actually adding it to the list of files.

--- src/main/java/utils/FileCrawler.java
+++ src/main/java/utils/FileCrawler.java
@@ -11,12 +11,33 @@ public class FileCrawler {
         this.baseFilePath = baseFilePath;
     }
 
-    public List<HashMap<String,String>> crawl(List<HashMap<String,String>> exisingFiles){
-        List<String> existingFilePaths = getPathsFromList(exisingFiles);
+    public List<HashMap<String,String>> crawl(List<HashMap<String,String>> existingFiles){
+        List<String> existingFilePaths = getPathsFromList(existingFiles);
         //recursively traverse file structure and build up a list of files. if there is a new path create a
         //new file entry with a new UUID.
-        return new ArrayList<>();
+        List<HashMap<String,String>> newFileList = crawler(existingFilePaths, new File(baseFilePath), existingFiles);
+
+        return newFileList;
     }
+    private List<HashMap<String,String>> crawler(List<String> knownPaths, File directory, List<HashMap<String,String>> existingFiles){
+        File[] files = directory.listFiles();
+        if(files!= null){
+            for(File file: files){
+                if(file.isDirectory()){
+                    crawler(knownPaths, file, existingFiles);
+                }else{
+                    if(!knownPaths.contains(file.getPath())){
+                        //create new entry for file, add to existing files;
+                    }
+                }
+
+            }
+        }
+        return existingFiles;
+
+    }
+
+
 
     private List<String> getPathsFromList(List<HashMap<String, String>> exisingFiles) {
         List<String> paths = new ArrayList<String>();
