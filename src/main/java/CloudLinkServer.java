47a9e3a4b32f0364255cd14e01aae94437725b84
bsolomons <44475261+TechCodeBlocks@users.noreply.github.com>
2021-03-27 19:09:54 +0000

    File Crawling system now mostly complete. Recursive function for crawling the directory works and has been tested. Remaining in this function is to add cloud functionality to add a new file listing to the online database. Will need to find a way of checking for deleted items:   Current thinking is that this could be done by comparing the new list with the old one and checking for paths that exist in the old one but not the new one and using a http request to delete these from the online db.   May require editing of cloud functions slightly

--- src/main/java/CloudLinkServer.java
+++ src/main/java/CloudLinkServer.java
@@ -1,5 +1,10 @@
 import com.microsoft.signalr.HubConnection;
 import com.microsoft.signalr.HubConnectionBuilder;
+import utils.FileCrawler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 //program needs to load a json map of file data and send it to the cloud.
 //This program map will not be changed or uploaded if no new files are detected
 //Message handling:
@@ -10,6 +15,21 @@ public class CloudLinkServer {
     static HubConnection hubConnection;
     static String url = "https://cloudlinkmessage.azurewebsites.net/api";
     public static void main(String[] args) {
+        FileCrawler fileCrawler = new FileCrawler("/Users/sandrarolfe/Documents/servertest");
+        List<HashMap<String,String>> filesEmpty = new ArrayList<>();
+        HashMap<String,String> testEntry1 = new HashMap<>();
+        testEntry1.put("path","/Users/sandrarolfe/Documents/servertest/Test1.rtf");
+        filesEmpty.add(testEntry1);
+        HashMap<String,String> testEntry2 = new HashMap<>();
+        testEntry2.put("path","/Users/sandrarolfe/Documents/servertest/test2/.DS_Store");
+        filesEmpty.add(testEntry2);
+        List<HashMap<String,String>> files = fileCrawler.crawl(filesEmpty);
+        for(HashMap<String,String> file : files){
+            System.out.println(file.get("_id"));
+            System.out.println(file.get("path"));
+            System.out.println(file.get("date-edited"));
+            System.out.println(file.get("online"));
+        }
         /*
         Connect to communications hub
         hubConnection = HubConnectionBuilder.create(url).build();
--- src/main/java/utils/FileCrawler.java
+++ src/main/java/utils/FileCrawler.java
@@ -1,9 +1,11 @@
 package utils;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class FileCrawler {
     String baseFilePath;
@@ -15,33 +17,45 @@ public class FileCrawler {
         List<String> existingFilePaths = getPathsFromList(existingFiles);
         //recursively traverse file structure and build up a list of files. if there is a new path create a
         //new file entry with a new UUID.
-        List<HashMap<String,String>> newFileList = crawler(existingFilePaths, new File(baseFilePath), existingFiles);
+        List<HashMap<String,String>> newFileList = crawler(existingFilePaths, new File(baseFilePath));
+        existingFiles.addAll(newFileList);
 
-        return newFileList;
+        return existingFiles;
     }
-    private List<HashMap<String,String>> crawler(List<String> knownPaths, File directory, List<HashMap<String,String>> existingFiles){
+    private List<HashMap<String,String>> crawler(List<String> knownPaths, File directory){
         File[] files = directory.listFiles();
+        List<HashMap<String,String>> newFileList = new ArrayList<>();
         if(files!= null){
             for(File file: files){
                 if(file.isDirectory()){
-                    crawler(knownPaths, file, existingFiles);
+                    newFileList.addAll(crawler(knownPaths, file));
                 }else{
+
                     if(!knownPaths.contains(file.getPath())){
+                        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                        System.out.println("Found a new file: " + file.getPath());
+                        HashMap<String,String> appendadbleValue = new HashMap<>();
+                        appendadbleValue.put("_id", UUID.randomUUID().toString());
+                        appendadbleValue.put("path", file.getPath());
+                        appendadbleValue.put("date-edited", sdf.format(file.lastModified()));
+                        appendadbleValue.put("online", "false");
+                        newFileList.add(appendadbleValue);
+
                         //create new entry for file, add to existing files;
                     }
                 }
 
             }
         }
-        return existingFiles;
+        return newFileList;
 
     }
 
 
 
-    private List<String> getPathsFromList(List<HashMap<String, String>> exisingFiles) {
+    private List<String> getPathsFromList(List<HashMap<String, String>> existingFiles) {
         List<String> paths = new ArrayList<String>();
-        for (HashMap<String, String> fileData: exisingFiles
+        for (HashMap<String, String> fileData: existingFiles
              ) {
             paths.add(fileData.get("path"));
 
